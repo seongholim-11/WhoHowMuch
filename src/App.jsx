f79d18d44853b6b7e5c405d05576f98834f65f11
seongho <damadu01@gmail.com>
2023-07-15 22:06:55 +0900

    내림차순, 금액 더하기 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,13 @@ function App() {
 
   // users 추가하고 삭제하는거 진행을 도와줄 state
   const [users, setUsers] = useState([]);
+  console.log("🚀 ~ file: App.jsx:18 ~ App ~ users:", users)
 
   // db의 users 컬렉션을 가져옴
   const usersCollectionRef = collection(db, "users");
 
-  // 유니크 id를 만들기 위한 useId();
-  const uniqueId = useId();
-
   // 추가될 금액을 위한 state
+  const [newMoney, setNewMoney] = useState("");
 
   // 시작될때 한번만 실행 // 읽어오기 - R
   useEffect(() => {
@@ -54,7 +53,7 @@ function App() {
     // 내가 업데이트 하고자 하는 db의 컬렉션의 id를 뒤지면서 데이터를 찾는다
     const userDoc = doc(db, "users", id)
     // 내가 업데이트 하고자 하는 key를 어떻게 업데이트할지 준비,, 중요한점이 db에는 문자열로 저장되어있다. 그래서 createUsers()함수안에서 money를 생성할때 숫자열로 형변환 해줘야한다
-    const newField = { howmuch: money + 1 };
+    const newField = { howmuch: money + Number(newMoney) };
     // updateDoc()을 이용해서 업데이트
     await updateDoc(userDoc, newField);
     // 화면 업데이트를 위한 state 변경
@@ -72,14 +71,15 @@ function App() {
   }
 
   // 띄워줄 데이터 key값에 고유ID를 넣어준다.
-  const showUsers = users.map((value) => (<div key={uniqueId}>
+  const showUsers = users.sort((a, b) => b.howmuch - a.howmuch).map((value) => (<div key={value.id}>
     <h1>Name: {value.name}</h1>
-    <h1>Age: {value.howmuch}</h1>
+    <h1>Money: {value.howmuch}원</h1>
     {/* 증가버튼은 이 안에 있어야지, 각기 다른 데이터마다 붙는다, users data를 map으로 돌기때문에, 그 안의 id랑 age를 넣어주면 된다.*/}
     {/* id를 넣어주는 이유는, 우리가 수정하고자 하는 데이터를 찾아야하기 때문에. */}
-    <input type="text" placeholder='금액을 입력해주세요' ㅐ/>
-    <button onClick={() => { updateUser(value.id, value.howmuch) }}>Increase Age</button>
-    <button onClick={() => { deleteUser(value.id) }}>Delete</button>
+    {/* 예를 들어, 두 명의 사용자가 있다고 가정해봅시다. 첫 번째 사용자의 ID가 "user1"이고 두 번째 사용자의 ID가 "user2"라고 가정하면, newMoney["user1"]은 첫 번째 사용자의 입력 값을 나타내고 newMoney["user2"]은 두 번째 사용자의 입력 값을 나타냅니다. */}
+    <input type="number" placeholder='금액을 입력해주세요'  value={newMoney[value.id]} onChange={(event) => { setNewMoney(event.target.value) }} />
+    <button onClick={() => { updateUser(value.id, value.howmuch) }}>금액 더하기</button>
+    <button onClick={() => { deleteUser(value.id) }}>삭제</button>
   </div>))
   return (
     <div className="App">
